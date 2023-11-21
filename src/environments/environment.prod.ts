613da6a29da5601cdde381a04b69ae6eda635180
Carlos Villavicencio <carlos.villavicencio.g7@gmail.com>
2023-11-21 12:11:20 -0500

    first version includes angular fire

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'sermaluc-cvv',
+    appId: '1:503919539493:web:aae2553663191f27df56cc',
+    storageBucket: 'sermaluc-cvv.appspot.com',
+    apiKey: 'AIzaSyAcYlpgzTJDD_me60FlY3wtsJSHcnMuarI',
+    authDomain: 'sermaluc-cvv.firebaseapp.com',
+    messagingSenderId: '503919539493',
+    measurementId: 'G-T77L6ZK62S',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'sermaluc-cvv',
+    appId: '1:503919539493:web:aae2553663191f27df56cc',
+    storageBucket: 'sermaluc-cvv.appspot.com',
+    apiKey: 'AIzaSyAcYlpgzTJDD_me60FlY3wtsJSHcnMuarI',
+    authDomain: 'sermaluc-cvv.firebaseapp.com',
+    messagingSenderId: '503919539493',
+    measurementId: 'G-T77L6ZK62S',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
